1e11f72bcdc4b40b609f8c6789b22338bb4211ca
Manoj Babu Yeluri <m.yeluri@zensar.com>
2022-05-05 23:21:09 +0530

    Revised Assignment_5

--- C#/Assignment-5/Assignment-5/Program.cs
+++ C#/Assignment-5/Assignment-5/Program.cs
@@ -6,71 +6,90 @@ using System.Threading.Tasks;
 
 namespace Assignment_5
 {
+    public class WithdrawException : ApplicationException
+    {
+        public WithdrawException(string m): base(m)
+        {
+        }
+    }
+    class Bank
+    {
+        string acc_name = "Manoj";
+        int amt= 25000;
+        static int newbal;
+        
+        public void Bank_Balance()
+        {
+            Console.WriteLine("Welcome! {0} your account balance is: {1}", acc_name,amt);
+         }
+        public void WithDraw()
+        {
+              Console.WriteLine("Enter the amount to withdraw: ");
+                int wm = int.Parse(Console.ReadLine());
+
+
+                newbal = amt - wm;
+                if (wm > amt)
+                {
+                    throw (new WithdrawException("Insufficient Amount please check your balance"));
+                }
+                else
+                {
+                    Console.WriteLine("Amount withdrawn successfully");
+                    Console.WriteLine("Remaining amount in your account is: {0}", newbal);
+                }
+            
+        }
+        public void Deposite()
+        {
+            try
+            {
+                Console.WriteLine("Enter Amount To Deposite: ");
+                int dm = int.Parse(Console.ReadLine());
+                newbal = amt + dm;
+                Console.WriteLine("Amount depositted successfully!");
+                Console.WriteLine("Total amount in your account is: {0}", newbal);
+            }
+            catch (Exception) { Console.WriteLine("Please enter valid amount"); }
+            
+        }
+    }
     internal class Program
     {
-       class Bank
-        {
-            string accname = "Manoj";
-            int amt = 10000;
-            static int newbal;
-            public void Bank_Balance()
-            {
-                { Console.WriteLine("Hello! {0} your account balance is: {1}", accname, amt); }
-
-            }
-            public void Deposite()
-            {
-                try
-                { Console.WriteLine("Enter the amount to deposite: "); 
-                    int dm = int.Parse(Console.ReadLine());
-                    newbal = amt + dm;
-                    Console.WriteLine("Amount depositted successfully!");
-                    Console.WriteLine("Total amount in your account is: {0}", newbal);
-                }
-                catch (Exception) { Console.WriteLine("Please enter Valid amount"); }
-            }
-            public void Withdraw()
-            {
-                try { Console.WriteLine("Enter amount to withdraw: ");
-                    int wm=int.Parse(Console.ReadLine());
-                    newbal = amt - wm;
-                    if (wm > amt) { Console.WriteLine("Insufficient Amount! Please check your balance"); }
-                    else { Console.WriteLine("Amount withdrawn successfully"); 
-                        Console.WriteLine("Remaining amount in your account is: {0}", newbal); }
-                }
-                catch (Exception) { Console.WriteLine("Please enter Valid amount."); }   
-                
-            }
-        }
         static void Main(string[] args)
         {
             Bank bank = new Bank();
-            string accname = "Manoj";
+            string acc_name = "Manoj";
             string bank_name = "Axis";
-            string accno;
-            int Pin;            // ------ Pin = 1234 ------
-            int action;
-            Console.WriteLine("Hi! {0} welcome to {1} bank", accname, bank_name);
-            Console.WriteLine("Please Enter the account number: ");
-            accno = Console.ReadLine();
-            Console.WriteLine("Welcome! {0}", accname);
-            Console.WriteLine("Please enter pin to access your account");
-            Pin = int.Parse(Console.ReadLine());
-            //try
+            string acc_num;
+            
+            Console.WriteLine("Hi! {0} Welcome to {1} bank", acc_name, bank_name);
+            Console.WriteLine("Please enter account number: ");
+            acc_num =Console.ReadLine();
+            Console.WriteLine("Enter 1 to check your Bank Balance");
+            Console.WriteLine("Enter 2 to withdraw amount from your account");
+            Console.WriteLine("Enter 3 to Deposite amount from your account");
+            Console.WriteLine("Select action to continue the process: ");
+            string action;
+            action=(Console.ReadLine());
+            try
             {
-                if (Pin == 1234)
-                { Console.WriteLine("Select the action to continue the process:"); }
-
-                else
-                { Console.WriteLine("Incorrect pin! Please check your pin and try again."); }
+                //bank.Bank_Balance();
+                //bank.WithDraw();
+                //bank.Deposite();
+                if (action == "1") { bank.Bank_Balance(); }
+                else if (action == "2") { bank.WithDraw(); }
+                else if (action == "3") { bank.Deposite(); }
+                else { Console.WriteLine("Please select valid action!"); }
             }
-           // catch(Exception) { Console.WriteLine("Unauthorised Access"); }
-            action=Convert.ToInt32(Console.ReadLine());
-            if (action == 1) { bank.Bank_Balance(); }
-            else if (action == 2) { bank.Deposite(); }
-            else if (action == 3) { bank.Withdraw(); }
-            else { Console.WriteLine("You have not selected any action! Try again."); }
-            Console.Read();
+            catch (WithdrawException w) { Console.WriteLine(w.Message); }
+            catch (FormatException e) { Console.WriteLine(e.Message); }
+            Console.ReadLine();
+            
+            
+            
+        } 
+     
         }
     }
-}
+
