c9d0912748321befd656c32204dfda62f7cca3c6
Manoj Babu Yeluri <m.yeluri@zensar.com>
2022-06-16 18:37:35 +0530

    CodeBaseTest4

---
+++ Code Base Test-4/CodeQue-2/CodeQue-2/Automobiles.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Automobiles.aspx.cs" Inherits="CodeQue_2.Automobiles" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body background="Pictures/Background-2.JFIF">
+    
+    <form id="form1" runat="server">
+        <div>
+        <h1 style="color:gold"><center>Hyper Cars and Bikes</center><br /></h1>
+            <br />
+        </div>
+        <h2 style="color:white;"><center> Select Bike or Hyper Car: <asp:DropDownList ID="DropDownList1" runat="server" Height="25px" Width="180px" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" AutoPostBack="True">
+            <asp:ListItem></asp:ListItem>        
+        </asp:DropDownList></center></h2>
+        <br />   
+        
+        <center><asp:Image ID="Image1" runat="server" height="300px" Width="550px" /></center>
+        <br />
+        <br />
+        <h2 style="color:white;"><center><asp:Label ID="Label1" runat="server" Text="Price Label"></asp:Label></center></h2>
+    </form>
+</body>
+</html>
---
+++ Code Base Test-4/CodeQue-2/CodeQue-2/Automobiles.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CodeQue_2
+{
+    public partial class Automobiles : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+
+                string[] str = new string[] {"Kawasaki H2r", "triumph", "lamborgini", "Lykan Hypersport", "Bugatti" };
+                for (int i = 0; i < str.Length; i++)
+                {
+                    DropDownList1.Items.Add(str[i]);
+                }
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string str = DropDownList1.Text;
+            Image1.ImageUrl = "~/Pictures/" + str + ".JFIF";
+
+
+            if (DropDownList1.Items.FindByText("Kawasaki H2r").Selected == true)
+            {
+                Label1.Text = "Price: 40,00,000/-";
+            }
+            else if (DropDownList1.Items.FindByText("triumph").Selected == true)
+            {
+                Label1.Text = "Price: 11,35,000/-";
+            }
+            else if (DropDownList1.Items.FindByText("lamborgini").Selected == true)
+            {
+                Label1.Text = "Price: 6,25,00,000/-";
+            }
+            else if (DropDownList1.Items.FindByText("Lykan Hypersport").Selected == true)
+            {
+                Label1.Text = "Price: 21,70,00,000/-";
+            }
+            else if (DropDownList1.Items.FindByText("Bugatti").Selected == true)
+            {
+                Label1.Text = "Price: 28,40,00,000/-";
+            }
+           
+
+        }
+    }
+}
